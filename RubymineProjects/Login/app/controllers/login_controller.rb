d2831bbbfd0b3fd3e1998ff7f2b2a11729e30724
Mathias Claesson <mc22ft@student.lnu.se>
2016-02-03 08:49:53 +0100

    set up form login/reg

--- RubymineProjects/Login/app/controllers/login_controller.rb
+++
@@ -1,7 +0,0 @@
-class LoginController < ApplicationController
-
-  def index
-    @users = User.all
-    #render :json => @users
-  end
-end
---
+++ RubymineProjects/Login/app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+
+
+  def index
+
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+        session[:userid] = @user.id
+        redirect_to apikey_path
+      else
+        render :action => 'new'
+    end
+
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:user_name, :email, :password)
+  end
+
+end
