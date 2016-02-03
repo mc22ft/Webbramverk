832f6263ce3f7fe66cc6444b9fa664ed088f0369
Mathias Claesson <mc22ft@student.lnu.se>
2016-02-04 00:45:58 +0100

    admin done

--- RubymineProjects/Login/app/controllers/sessions_controller.rb
+++ RubymineProjects/Login/app/controllers/sessions_controller.rb
@@ -7,9 +7,10 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      log_in user
-      remember user
-      redirect_to user
+      redirect_back_or user
+      #log_in user
+      #remember user
+      #redirect_to user
       # Log the user in and redirect to the user's show page.
     else
       # Create an error message.
--- RubymineProjects/Login/db/schema.rb
+++ RubymineProjects/Login/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160203195319) do
+ActiveRecord::Schema.define(version: 20160203221847) do
 
   create_table "apps", force: :cascade do |t|
     t.integer  "user_id"
@@ -24,12 +24,13 @@ ActiveRecord::Schema.define(version: 20160203195319) do
   add_index "apps", ["user_id"], name: "index_apps_on_user_id"
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",            null: false
-    t.string   "email",           null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string   "name",                            null: false
+    t.string   "email",                           null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
