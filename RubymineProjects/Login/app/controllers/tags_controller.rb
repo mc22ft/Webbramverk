80e66d1377bbf94107cb0ec1e80639a8b1ea1684
Mathias Claesson <mc22ft@student.lnu.se>
2016-02-26 00:27:24 +0100

    remove code

--- RubymineProjects/Login/app/controllers/tags_controller.rb
+++ RubymineProjects/Login/app/controllers/tags_controller.rb
@@ -1,18 +1,3 @@
 class TagsController < ApplicationController
 
-
-  def index
-
-    #search for tags name
-    #if params[:search].present?
-    #  @tags = Tag.search(params[:search]).limit(@limit).offset(@offset).order('created_at DESC')
-    #else
-    #  @tags = Tag.all
-    #end
-  end
-
-  def show
-   # @tag = Tag.find(params[:id])
-  end
-
 end
